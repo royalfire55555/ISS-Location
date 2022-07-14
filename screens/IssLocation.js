a91b465c787e0511b65df15772b091918eb17520
SammmE <101365537+SammmE@users.noreply.github.com>
2022-07-13 21:12:54 -0400

    added iss location and map

--- screens/IssLocation.js
+++ screens/IssLocation.js
@@ -1,15 +1,110 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image,
+    Alert,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import axios from "axios";
 
 export default class IssLocation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: {},
+        };
+    }
+
+    componentDidMount() {
+        this.getIssLocation();
+    }
+
+    // api: https://api.wheretheiss.at/v1/satellites/25544
+    getIssLocation = () => {
+        axios
+            .get("https://api.wheretheiss.at/v1/satellites/25544")
+            .then((response) => {
+                this.setState({ location: response.data() });
+            })
+            .catch((error) => {
+                Alert.alert(error.message);
+            });
+    };
+
     render() {
-        return (
-            <View style={styles.container}>
-                <Text>ISS location Screen</Text>
-                <StatusBar style="auto" />
-            </View>
-        );
+        if (Object.keys(this.state.location).length === 0) {
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Text>Loading...</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.container}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <ImageBackground
+                        source={require("../assets/iss_bg.jpg")}
+                        style={styles.backgroundImage}
+                    >
+                        <View style={styles.titleContainer}>
+                            <Text style={styles.titleText}>
+                                ISS location Screen
+                            </Text>
+                        </View>
+                        <View style={styles.mapContainer}>
+                            <MapView
+                                style={styles.map}
+                                region={{
+                                    latitude: this.state.location.latitude,
+                                    longitude: this.state.location.longitude,
+                                    latitudeDelta: 100,
+                                    longitudeDelta: 100,
+                                }}
+                            >
+                                <Marker
+                                    coordinate={{
+                                        latitude: this.state.location.latitude,
+                                        longitude:
+                                            this.state.location.longitude,
+                                    }}
+                                >
+                                    <Image
+                                        source={require("../assets/iss_icon.png")}
+                                        style={{ width: 50, height: 50 }}
+                                    />
+                                </Marker>
+                            </MapView>
+                        </View>
+                        <View style={styles.infoContainer}>
+                            <Text style={styles.infoText}>
+                                Latitude: {this.state.location.latitude}
+                            </Text>
+                            <Text style={styles.infoText}>
+                                Longitude: {this.state.location.longitude}
+                            </Text>
+                            <Text style={styles.infoText}>
+                                Altitude: {this.state.location.altitude}
+                            </Text>
+                            <Text style={styles.infoText}>
+                                Velocity: {this.state.location.velocity}
+                            </Text>
+                        </View>
+                    </ImageBackground>
+                </View>
+            );
+        }
     }
 }
 
@@ -19,5 +114,42 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: "cover",
+    },
+    titleText: {
+        fontWeight: "bold",
+        fontSize: 30,
+        color: "white",
+    },
+    titleContainer: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    mapContainer: {
+        flx: 0.6,
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: "white",
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30,
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold",
     },
 });
